d67d9da225652538f737206a18fbc166c5ffa848
weizhanzhan <375786117@qq.com>
2023-12-26 23:23:19 +0800

    update

--- docs/guide/sidebar.js
+++ docs/guide/sidebar.js
@@ -1,44 +1,44 @@
 module.exports = [{
-		title: "第一章节",
+		title: "第一章",
 		path: '/guide/notes/1'
 	}, {
-		title: "第二章节",
+		title: "第二章",
 		path: '/guide/notes/2'
 	}, {
-		title: "第三章节",
+		title: "第三章",
 		path: '/guide/notes/3'
 	}, {
-		title: "第四章节",
+		title: "第四章",
 		path: '/guide/notes/4'
 	}, {
-		title: "第五章节",
+		title: "第五章 用CSS设置文本样式",
 		path: '/guide/notes/5'
 	}, {
-		title: "第六章节",
+		title: "第六章 设置图片效果",
 		path: '/guide/notes/6'
 	}, {
-		title: "第七章节",
+		title: "第七章",
 		path: '/guide/notes/7'
 	}, {
-		title: "第八章节",
+		title: "第八章",
 		path: '/guide/notes/8'
 	}, {
-		title: "第九章节",
+		title: "第九章",
 		path: '/guide/notes/9'
 	}, {
-		title: "第十章节 Javascript基础",
+		title: "第十章 Javascript基础",
 		path: '/guide/notes/10'
 	}, {
-		title: "第十一章节 程序控制流与函数",
+		title: "第十一章 程序控制流与函数",
 		path: '/guide/notes/11'
 	}, {
-		title: "第十二章节 Javascript中的对象",
+		title: "第十二章 Javascript中的对象",
 		path: '/guide/notes/12'
 	}, {
-		title: "第十三章节 以集合方式处理数据",
+		title: "第十三章 以集合方式处理数据",
 		path: '/guide/notes/13'
 	}, {
-		title: "第十四章节 DOM",
+		title: "第十四章 DOM",
 		path: '/guide/notes/14'
 	}
 	// {
